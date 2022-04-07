2afcba483e8643d03ab2c8be51c02d9367db58c8
LydiaYan628 <zy1562@nyu.edu>
2022-04-07 20:41:31 +0400

    touch

---
+++ Code/touch.py
@@ -0,0 +1,22 @@
+# NYUAD IM
+# Desert Media Art
+# Modified by Lydia Yan
+#
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+# SPDX-License-Identifier: MIT
+#
+# Reference: https://learn.adafruit.com/circuitpython-essentials/circuitpython-cap-touch
+
+"""CircuitPython Essentials Capacitive Touch example"""
+import time
+import board
+import touchio
+
+touch_pad0 = board.A0
+
+touch0 = touchio.TouchIn(touch_pad0)
+
+while True:
+    if touch0.value:
+        print("A0 Touched!")
+    time.sleep(0.05)
